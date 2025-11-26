6f1bad6b63043b4dab2d8dfe8c51f15517348b92
Bob Torgerson <rltorgerson@alaska.edu>
2025-11-26 14:06:25 -0900

    Removes all Bulma classes and removes styles from all elements.

--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,32 @@
 <template>
-  <div class="hero is-fullheight">
-    <div class="hero-body">
-      <div class="container">
-        <div v-if="!mapStore.selectedLocation" class="has-text-centered">
-          <h1 class="title is-1 mb-6">Welcome to the Kuti First Draft</h1>
-          <p class="subtitle is-3 mb-6">Select your Alaska location</p>
+  <div>
+    <div>
+      <div v-if="!mapStore.selectedLocation">
+        <h1>Welcome to the Kuti First Draft</h1>
+        <p>Select your Alaska location</p>
 
-          <div class="buttons is-centered">
-            <button
-              @click="mapStore.setLocation('Craig')"
-              class="button is-info is-large mx-3"
-              style="min-width: 200px; min-height: 80px"
-            >
-              <span class="is-size-4">Craig</span>
-            </button>
+        <div>
+          <a @click.prevent="mapStore.setLocation('Craig')" href="#">Craig</a>
+          &nbsp;
+          <a @click.prevent="mapStore.setLocation('Kassan')" href="#">Kassan</a>
+        </div>
+      </div>
 
-            <button
-              @click="mapStore.setLocation('Kassan')"
-              class="button is-success is-large mx-3"
-              style="min-width: 200px; min-height: 80px"
+      <div v-else>
+        <div>
+          <div>
+            <h1>{{ mapStore.selectedLocation }}, Alaska</h1>
+          </div>
+          <div>
+            <a @click.prevent="mapStore.switchLocation" href="#"
+              >Switch Location</a
             >
-              <span class="is-size-4">Kassan</span>
-            </button>
           </div>
         </div>
 
-        <div v-else>
-          <div class="level mb-6">
-            <div class="level-left">
-              <div class="level-item">
-                <h1 class="title is-2">
-                  {{ mapStore.selectedLocation }}, Alaska
-                </h1>
-              </div>
-            </div>
-            <div class="level-right">
-              <div class="level-item">
-                <button
-                  @click="mapStore.switchLocation"
-                  class="button is-light"
-                >
-                  Switch Location
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <RiskLevel />
-          <Map />
-          <Resources />
-        </div>
+        <RiskLevel />
+        <Map />
+        <Resources />
       </div>
     </div>
   </div>
