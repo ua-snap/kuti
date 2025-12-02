0511748ea93138b8055705fe054454349a4a342f
Bob Torgerson <rltorgerson@alaska.edu>
2025-12-02 10:45:56 -0900

    Adds Umami analytics to site.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,18 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@pinia/nuxt"],
   pages: true,
+  app: {
+    head: {
+      script: [
+        {
+          src: "https://umami.snap.uaf.edu/script.js",
+          async: true,
+          defer: true,
+          "data-website-id": "57573294-19a7-419c-84d1-b8d3e7c3bc16",
+          "data-domains": "aklandslides.org",
+          "data-do-not-track": "true",
+        },
+      ],
+    },
+  },
 });
