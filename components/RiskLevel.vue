7cc72b9817a74c3c2a4c043e2e5e4b4de669f2ef
Bob Torgerson <rltorgerson@alaska.edu>
2026-01-28 15:05:30 -0900

    Uses the new API return data to populate the risk levels.

--- components/RiskLevel.vue
+++ components/RiskLevel.vue
@@ -2,7 +2,9 @@
   <div v-if="landslideApiStore.data">
     <h2>
       {{
-        landslideApiStore.getRiskLevelText(landslideApiStore.data.risk_level)
+        landslideApiStore.getRiskLevelText(
+          landslideApiStore.data.realtime_risk_level,
+        )
       }}
       risk of landslide now
     </h2>
@@ -17,31 +19,41 @@
 
     <h3>24 hour forecast</h3>
     <p>
-      {{ landslideApiStore.getRiskLevelText(landslideApiStore.data.risk_24hr) }}
+      {{
+        landslideApiStore.getRiskLevelText(
+          landslideApiStore.data["block_24hr"].risk_level,
+        )
+      }}
       risk for the next 24 hours
     </p>
     <p>
       Precipitation:
-      {{ landslideApiStore.data.precipitation_inches.toFixed(2) }}"
+      {{ landslideApiStore.data.realtime_rainfall_mm }} mm
     </p>
 
     <h3>3 day forecast</h3>
     <div>
       <h4>24 hours</h4>
       <span>{{
-        landslideApiStore.getRiskLevelText(landslideApiStore.data.risk_24hr)
+        landslideApiStore.getRiskLevelText(
+          landslideApiStore.data["block_24hr"].risk_level,
+        )
       }}</span>
     </div>
     <div>
       <h4>2 days</h4>
       <span>{{
-        landslideApiStore.getRiskLevelText(landslideApiStore.data.risk_2days)
+        landslideApiStore.getRiskLevelText(
+          landslideApiStore.data["block_2days"].risk_level,
+        )
       }}</span>
     </div>
     <div>
       <h4>3 days</h4>
       <span>{{
-        landslideApiStore.getRiskLevelText(landslideApiStore.data.risk_3days)
+        landslideApiStore.getRiskLevelText(
+          landslideApiStore.data["block_3days"].risk_level,
+        )
       }}</span>
     </div>
   </div>
