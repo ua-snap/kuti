c20bec13cf9b3b985176db87a7fec85c03945fe5
Bob Torgerson <rltorgerson@alaska.edu>
2026-04-06 08:26:33 -0800

    Disables dark mode on mobile devices.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
-  css: ["bulma/css/bulma.min.css", "leaflet/dist/leaflet.css"],
+  css: [
+    "bulma/css/bulma.min.css",
+    "leaflet/dist/leaflet.css",
+    "~/assets/css/main.css",
+  ],
   devtools: { enabled: true },
   modules: ["@pinia/nuxt"],
   pages: true,
   app: {
     head: {
+      meta: [
+        {
+          name: "color-scheme",
+          content: "light",
+        },
+      ],
       script: [
         {
           src: "https://umami.snap.uaf.edu/script.js",
