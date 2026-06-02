3ef56e636b11ada8cc4eb15115262b113d76b617
Bob Torgerson <rltorgerson@alaska.edu>
2026-06-02 09:31:55 -0800

    Adds SEO / OpenGraph tags. (#60)
    * Adds SEO / OpenGraph tags.
    
    * Adds preview.png image for OpenGraph.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+var metas = {
+  title: "Alaska Landslide Risk - Prince of Wales Island Communities",
+  description:
+    "Real-time landslide risk assessment for Prince of Wales Island communities in Alaska.",
+  keywords:
+    "Alaska landslides, Prince of Wales Island, Craig Alaska, Kasaan Alaska, landslide risk, landslide preparedness",
+  preview: "/preview.png",
+  url: "https://powlandslides.org",
+};
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   css: ["leaflet/dist/leaflet.css", "~/assets/css/main.scss"],
@@ -9,13 +19,21 @@ export default defineNuxtConfig({
     head: {
       htmlAttrs: {
         "data-theme": "light",
+        lang: "en",
       },
+      title: "Prince of Wales Island Landslide Risk",
       meta: [
-        {
-          name: "color-scheme",
-          content: "light",
-        },
+        { name: "description", content: metas.description },
+        { name: "keywords", content: metas.keywords },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { property: "og:type", content: "website" },
+        { property: "og:site_name", content: metas.title },
+        { property: "og:title", content: metas.title },
+        { property: "og:description", content: metas.description },
+        { property: "og:url", content: metas.url },
+        { property: "og:image", content: metas.preview },
       ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
       script: [
         {
           src: "https://umami.snap.uaf.edu/script.js",
